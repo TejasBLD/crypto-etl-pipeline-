484e8429ba71c3250bea857ac29f9e07f294719a
Tejas Singh <worktejas2033@gmail.com>
2025-10-04 16:48:27 +0530

    Add files via upload

--- extract.py
+++ extract.py
@@ -1,18 +1,39 @@
 import pandas as pd
 import requests
+import schedule
+import time
+from datetime import datetime,timezone
+import os
 
 def fetching_data():
-    url = "https://api.coingecko.com/api/v3/coins/markets"
-    params={
-        "vs_currency":"inr",
-        "ids":"Bitcoin,ethereum"
+    print(f"fetching data at {datetime.now(timezone.utc)}...")
+    try:
+        
+        url = "https://api.coingecko.com/api/v3/coins/markets"
+        params={
+            "vs_currency":"inr",
+            "ids":"Bitcoin,ethereum"
     }
-    response=requests.get(url,params=params)
-    data=response.json()
-    df=pd.DataFrame(data)
-    return df
+        response=requests.get(url,params=params)
+        response.raise_for_status()
+        data=response.json()
+        df=pd.DataFrame(data)
+        cols=["id","symbol","current_price","market_cap","total_volume","last_updated"]
+        df=df[cols]
+        df["fetched_at"]=datetime.now(timezone.utc)
+    
+        header= not os.path.exists('crypto_price.csv')
+        df.to_csv("crypto_price.csv", index=False,mode="a",header=header)
+        print(f"Data saved to crypto_price.csv")
+    except Exception as e:
+        print(f"ERROR in TRANSFORMING & LOADING JOB :{e}")
 if __name__=="__main__":
-    df=fetching_data()
-    print(df.head())
+    schedule.every(5).minutes.do(fetching_data)
+    print("Extractor started. Will run every 5 minutes.")
+    
+    fetching_data()
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
     
         
--- load.py
+++ load.py
@@ -3,8 +3,11 @@ from transform import transform_data
 from extract import fetching_data
 from dotenv import load_dotenv
 import os
+import schedule
+import time
+import pandas as pd
 
-load_dotenv()  
+load_dotenv()
 
 DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
@@ -12,14 +15,28 @@ DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 
-def load_data(df):
+engine = create_engine(f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
+csv_file_path='crypto_price.csv'
 
-    engine = create_engine(f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
-
-    df.to_sql('crypto_prices',engine,if_exists='append',index=False)
-    print("Data loaded successfully !")
-    
+def etl_job():
+    try:
+        if not os.path.exists(csv_file_path) or os.path.getsize(csv_file_path)==0:
+            print("CSV file is empty or does not exists.Skipping")
+            return
+        raw_df=pd.read_csv(csv_file_path)
+        transformed_df= transform_data(raw_df)
+        transformed_df.to_sql('crypto_prices',engine,if_exists='append',index=False)
+        print(f"Successfully loaded {len(transformed_df)} rows to the database")
+        
+        os.remove(csv_file_path)
+        print("Cleared crypto_price.csv for the next batch.")
+    except pd.errors.EmptyDataError:
+        print("CSV file is empty.Nothing to process.")
+    except Exception as e:
+        print(f"Error in transform & load job :{e}")
 if __name__=="__main__":
-    raw_df = fetching_data()
-    df=transform_data(raw_df)
-    load_data(df)
+    schedule.every(5).minutes.do(etl_job)
+    print("Transform-load process started. Will run every 5 minutes")
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
